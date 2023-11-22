25ae8b3a523bcb117f682429d0343e0532a8fe9c
Сергей Константинов <sokonstantinov@sberbank.ru>
2023-11-22 15:24:07 +0300

    feat: добавлена базовая настройка сцены

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import Phaser from 'phaser';
+
+import { scenes } from './scenes';
+import { isDevelopment, isProduction } from './utils';
+
+/**
+ * https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
+ */
+new Phaser.Game({
+  width: 800, // 1024
+  height: 600, // 768
+  title: 'Phaser RPG',
+  url: import.meta.env.URL || '',
+  version: import.meta.env.VERSION || '0.0.1',
+  scene: scenes,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: isDevelopment,
+    },
+  },
+  disableContextMenu: isProduction,
+  backgroundColor: '#000',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  pixelArt: true,
+});
